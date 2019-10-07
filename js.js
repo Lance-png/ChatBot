2dfdb10f2375f3aedc207ccb41670fe1b5e017d1
charlielowe <clowe@salisbury6c.ac.uk>
2019-10-07 15:40:23 +0100

    hello

--- js.js
+++ js.js
@@ -1,8 +1,19 @@
-var ask=document.getElementById("ask").value;
-switch(ask) {
-    case ("tea"):
-        var print=document.querySelector(ans);
-        ask=
-    break;
-    case ("coffee")
-}
+var ask=document.getElementById("ask");
+var print=document.querySelector("#ans");
+var button=document.querySelector("#click");
+button.addEventListener("click", function (element) {
+ 
+    switch(ask.value) {
+        case "tea":
+            print.innerHTML=("Tea? go make yourself tea then");
+        break;
+        case "coffee":
+            print.innerHTML=("Coffee? go make yourself coffee then");
+            break;
+        default:
+            print.innerHTML=("Enter tea or coffee");
+            break;
+    
+    }
+   
+});
